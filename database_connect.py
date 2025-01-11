50099dcd5d237586d72ddae5596460c7c1fa9dbb
winterholic <iuholic83@naver.com>
2025-01-11 19:53:12 +0900

    Feat : mysql 연결 세팅 추가

---
+++ database_connect.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "mysql+aiomysql://root:0000@localhost:3306/bbibbiyakton"
+
+# SQLAlchemy 비동기 엔진 생성
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+# 세션 팩토리 생성
+SessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+# 종속성 함수
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from fastapi import FastAPI, Request
+from fastapi import Depends, FastAPI, Request
 from datetime import datetime
+from sqlalchemy import text
 import logging
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
 
 # 모듈 추가
 from app.routers.test_router import router as test_router
 from app.utils.jwt_token_generator import router as jwt_token_generator
 from jwt_middleware import JWTMiddleware, BlockUndefinedRoutesMiddleware
+from database_connect import get_db
 
 app = FastAPI(title="Hackathon Project", version="1.0")
 
@@ -18,7 +22,7 @@ app.include_router(jwt_token_generator)
 
 # 허용된 경로 및 접두사 설정
 allowed_routes = []
-excluded_prefixes = ["/public", "/static", "/docs", "/redoc", "/openapi.json"]
+excluded_prefixes = ["/", "/users" ,"/public", "/static", "/docs", "/redoc", "/openapi.json"]
 
 # 미들웨어 추가
 app.add_middleware(JWTMiddleware,
@@ -43,8 +47,12 @@ async def log_requests(request: Request, call_next):
     logging.debug(f"Response Status: {response.status_code}")
     return response
 
+@app.get("/users/") #테스트용 모든 users데이터 전부 조회
+async def read_users(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(text("SELECT * FROM users"))  # 모든 데이터를 조회
+    users = [dict(row._mapping) for row in result.fetchall()]  # 딕셔너리로 변환
+    return users
 
-
-#pip install fastapi uvicorn aioredis pymysql sqlalchemy databases PYJWT python-dotenv pydantic-settings starlette
+#pip install fastapi uvicorn aioredis pymysql sqlalchemy databases PYJWT python-dotenv pydantic-settings starlette aiomysql
 #uvicorn main:app --reload
 #pip install --upgrade fastapi
