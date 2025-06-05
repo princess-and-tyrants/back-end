77830a3dd04d72ee3c09a4436d95a515fc67f065
winterholic <iuholic83@naver.com>
2025-06-05 15:48:32 +0900

    Fix : ci/cd 수정

--- app/routers/home_router.py
+++ app/routers/home_router.py
@@ -19,8 +19,8 @@ async def get_home_profile(request: Request, user_id: str, db: AsyncSession = De
     result = await user_service.get_home_profile(user_id)
     return result
 
-@router.get("/home/profile/my", dependencies=[verify_header()], summary="유저 기본 정보 조회 api", description="", tags=["Home"])
-async def get_home_profile(request: Request, db: AsyncSession = Depends(get_db)):
+@router.get("/home/profile/my", dependencies=[verify_header()], summary="유저 본인 기본 정보 조회 api", description="", tags=["Home"])
+async def get_home_my_profile(request: Request, db: AsyncSession = Depends(get_db)):
     user_service = UserService(db)
     user = getattr(request.state, "user", None)
     if not user:
