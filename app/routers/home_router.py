c35d8e671d69ba8c896ef28d7ff7a483778ba807
winterholic <iuholic83@naver.com>
2025-06-11 15:16:23 +0900

    Fix : 경로 수정

--- app/routers/home_router.py
+++ app/routers/home_router.py
@@ -11,11 +11,9 @@ def verify_header(access_token=Security(APIKeyHeader(name='Authorization'))):
 router = APIRouter()
 
 @router.get("/home/profile", dependencies=[verify_header()], summary="유저 기본 정보 조회 api", description="", tags=["Home"])
-async def get_home_profile(request: Request, user_id: str, db: AsyncSession = Depends(get_db)):
+async def get_home_profile(user_id: str, db: AsyncSession = Depends(get_db)):
     user_service = UserService(db)
-    user = getattr(request.state, "user", None)
-    if not user:
-        raise HTTPException(status_code=401, detail="Unauthorized")
+    
     result = await user_service.get_home_profile(user_id)
     return result
 
